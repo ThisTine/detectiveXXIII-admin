1fa57e983e220ef094ed50a172c7b3bcdd8e1df4
I_AM_TINE <sutichok46@gmail.com>
2022-07-22 16:16:47 +0700

    fix event group

--- src/views/admin/event/components/EventGroup.jsx
+++ src/views/admin/event/components/EventGroup.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Badge, Box, Button, Heading, HStack, useBoolean, VStack } from '@chakra-ui/react'
+import { Badge, Box, Button, Heading, HStack, Stack, useBoolean, VStack } from '@chakra-ui/react'
 import Swal from 'sweetalert2'
 
 import Card from 'components/card/Card'
@@ -31,9 +31,9 @@ const EventGroup = ({ item }) => {
         <Box w="100%">
           <Badge colorScheme={"blue"} size="sm">{item.id}</Badge>
           {!isPresenting && <Heading size="md" >Hints</Heading>}
-          {!isPresenting && <HStack alignItems={"flex-start"}> {item.hints.map(item => <Badge colorScheme={"yellow"} key={item.id} >{item.location}</Badge>)} </HStack>}
-          <Heading size="md" >Users</Heading>
-          <HStack alignItems={"flex-start"}> {item.users.map(item => <Badge colorScheme={"purple"} key={item.id} >{item.name}</Badge>)} </HStack>
+          {!isPresenting && <HStack alignItems={"flex-start"} flexWrap="wrap" gap={2}> {item.hints.map(item => <Badge colorScheme={"yellow"} key={item.id} >{item.location}</Badge>)} </HStack>}
+          <Heading size="md" py={2} >Users</Heading>
+          <Stack alignItems={"flex-start"} flexWrap="wrap" gap={2} > {item.users.map(item => <Badge colorScheme={"purple"} key={item.id} >{item.name}</Badge>)} </Stack>
         </Box>
 
         <Button colorScheme={"red"} onClick={deleteEventGroup} isLoading={isLoading}>Delete</Button>
